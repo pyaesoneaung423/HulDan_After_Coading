09d6d8a360c07c740992e2b42aca5927943e9f33
pyae sone aung <115071081+pyaesoneaung423@users.noreply.github.com>
2022-12-05 01:20:38 +0630

    Add files via upload

---
+++ HluDan_Code_Assets_after_Coding/js/index.js
@@ -0,0 +1,28 @@
+var elements = document.querySelectorAll(
+  "header > #header_wrapper > nav > ul > li > a"
+);
+
+console.log("Elements", elements);
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+for (var i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+
+    current[0].className = current[0].className.replace("active", "");
+    this.className += " active";
+  });
+}
